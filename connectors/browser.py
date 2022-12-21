421b2f6e92b69980c94e6c8dcc69d25a1f200a74
itsComcast <nguyen.2274@osu.edu>
2022-12-20 20:53:20 -0500

    Removed unneccessary browser.py

--- connectors/browser.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from selenium import webdriver
-
-# TURN INTO CONFIG
-DRIVER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "chromedriver")
-
-def create_driver():
-    return webdriver.Chrome(DRIVER_PATH)
